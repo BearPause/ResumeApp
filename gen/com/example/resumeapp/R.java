7b78749ebba3846ff1b5cfa942fabb11806ed151
Caleb Weaver <calebweaver59@gmail.com>
2014-02-07 11:08:11 -0500

    Added education text

--- gen/com/example/resumeapp/R.java
+++ gen/com/example/resumeapp/R.java
@@ -33,8 +33,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
+        public static final int action_settings=0x7f090002;
         public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,6 +46,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int education=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int main_title=0x7f050003;
     }
@@ -72,7 +74,6 @@ public final class R {
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
     }
